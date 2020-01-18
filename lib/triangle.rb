abcde978794c6fe66da53430ef85926ddf7dda86
Cesar <cesarmota@ymail.com>
2020-01-18 14:52:16 -0600

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
-  # write code here
+    attr_reader :side_two,:side_one, :side_three
+  def initialize(side_two,side_one, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+  end
+
+  def kind
+    validate_triangle
+    # if @side_one + @side_two < @side_three || @side_two + @side_three < @side_one || @side_three + @side_one < @side_two
+    #   # begin
+    #     raise TriangleError
+    #   # # rescue  TriangleError=> error 
+    #   # #   puts error.message
+    #  # end
+        
+    if @side_one == side_two && @side_two == side_three
+    :equilateral
+    elsif  @side_one == @side_two || @side_three == @side_one  || @side_two == @side_three
+    :isosceles 
+    else 
+    :scalene
+    end
+  end
+
+    def validate_triangle
+      real_triangle = [(@side_one + @side_two > @side_three),(@side_two + @side_three > @side_one ),(@side_three + @side_one > @side_two)]
+      [@side_one,@side_two, @side_three].each do |side|
+        real_triangle << false if side <= 0
+        raise TriangleError if real_triangle.include?(false)
+      end
+      end
+  class TriangleError < StandardError
+    # def message
+    #   "triangles violating triangle inequality are illegal"
+    # end
+  end
+
 end
